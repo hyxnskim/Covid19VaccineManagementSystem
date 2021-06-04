69f6da8a969fc2c713ac461756407e62f75fb270
hyxnskim <hskim9706@gmail.com>
2021-06-04 15:15:35 +0900

    Initiate Center and Member classes

--- Covid19VaccineManagementSystem/src/model/dto/Center.java
+++ Covid19VaccineManagementSystem/src/model/dto/Center.java
@@ -11,4 +11,197 @@ package model.dto;
 
 public class Center {
 
+	/** 센터명, 필수 */
+	private String centerName;
+	
+	/** 시설명, 필수 */
+	private String facName;
+	
+	/** 우편번호, 필수 */
+	private String postCode;
+	
+	/** 지역, 필수 */
+	private String district;
+	
+	/** 주소, 필수 */
+	private String address;
+	
+	/** 연락처, 선택 */
+	private String contact;
+
+	/** 기본 생성자	 */
+	public Center() {}
+
+	/**
+	 * <pre>
+	 * 필수 입력정보 생성자
+	 * </pre>
+	 * @param centerName 센터명
+	 * @param facName 시설명
+	 * @param postCode 우편번호
+	 * @param district 지역
+	 * @param address 주소
+	 */
+	public Center(String centerName, String facName, String postCode, String district, String address) {
+		this.centerName = centerName;
+		this.facName = facName;
+		this.postCode = postCode;
+		this.district = district;
+		this.address = address;
+	}
+
+	/**
+	 * <pre>
+	 * 필수 입력정보 생성자
+	 * </pre>
+	 * @param centerName 센터명
+	 * @param facName 시설명
+	 * @param postCode 우편번호
+	 * @param district 지역
+	 * @param address 주소
+	 * @param contact 연락처
+	 */
+	public Center(String centerName, String facName, String postCode, String district, String address, String contact) {
+		this(centerName, facName, postCode, district, address);
+		this.contact = contact;
+	}
+
+	/**
+	 * @return the centerName
+	 */
+	public String getCenterName() {
+		return centerName;
+	}
+
+	/**
+	 * @param centerName the centerName to set
+	 */
+	public void setCenterName(String centerName) {
+		this.centerName = centerName;
+	}
+
+	/**
+	 * @return the facName
+	 */
+	public String getFacName() {
+		return facName;
+	}
+
+	/**
+	 * @param facName the facName to set
+	 */
+	public void setFacName(String facName) {
+		this.facName = facName;
+	}
+
+	/**
+	 * @return the postCode
+	 */
+	public String getPostCode() {
+		return postCode;
+	}
+
+	/**
+	 * @param postCode the postCode to set
+	 */
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+	/**
+	 * @return the district
+	 */
+	public String getDistrict() {
+		return district;
+	}
+
+	/**
+	 * @param district the district to set
+	 */
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the contact
+	 */
+	public String getContact() {
+		return contact;
+	}
+
+	/**
+	 * @param contact the contact to set
+	 */
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((centerName == null) ? 0 : centerName.hashCode());
+		result = prime * result + ((postCode == null) ? 0 : postCode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Center other = (Center) obj;
+		if (centerName == null) {
+			if (other.centerName != null)
+				return false;
+		} else if (!centerName.equals(other.centerName))
+			return false;
+		if (postCode == null) {
+			if (other.postCode != null)
+				return false;
+		} else if (!postCode.equals(other.postCode))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Center [centerName=");
+		builder.append(centerName);
+		builder.append(", facName=");
+		builder.append(facName);
+		builder.append(", postCode=");
+		builder.append(postCode);
+		builder.append(", district=");
+		builder.append(district);
+		builder.append(", address=");
+		builder.append(address);
+		builder.append(", contact=");
+		builder.append(contact);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+
+	
 }
--- Covid19VaccineManagementSystem/src/model/dto/Member.java
+++ Covid19VaccineManagementSystem/src/model/dto/Member.java
@@ -13,4 +13,217 @@ package model.dto;
  */
 public class Member {
 
+	/** 이름, 필수 */
+	private String name;
+
+	/** 주민번호, 필수, 식별키 */
+	private String regiNum;
+	
+	/** 연락처, 선택 */
+	private String contact;
+	
+	/** 지역, 필수 */
+	private String district;
+	
+	/** 백신 종류, 필수 */
+	private String vacType;
+	
+	/** 1차 접종일, 필수 */
+	private String dateFirst;
+	
+	/** 2차 접종 예정일 */
+	private String dateSecond;
+	
+	/** 2차 접종 알림일 */
+	private String notiDate;
+
+	/** 기본 생성자	 */
+	public Member() {}
+
+	/**
+	 * <pre>
+	 * 필수 정보 입력 생성자
+	 * </pre>
+	 * @param name 이름
+	 * @param regiNum 주민번호
+	 * @param contact 연락처
+	 * @param district 지역
+	 * @param vacType 접종 백신 종료
+	 * @param dateFirst 1차 접종일
+	 */
+	public Member(String name, String regiNum, String contact, String district, String vacType, String dateFirst) {
+		this.name = name;
+		this.regiNum = regiNum;
+		this.contact = contact;
+		this.district = district;
+		this.vacType = vacType;
+		this.dateFirst = dateFirst;
+		
+		// TODO : dateSecond, notiDate 계산하는 메서드 구현해서 추가하기
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the regiNum
+	 */
+	public String getRegiNum() {
+		return regiNum;
+	}
+
+	/**
+	 * @param regiNum the regiNum to set
+	 */
+	public void setRegiNum(String regiNum) {
+		this.regiNum = regiNum;
+	}
+
+	/**
+	 * @return the contact
+	 */
+	public String getContact() {
+		return contact;
+	}
+
+	/**
+	 * @param contact the contact to set
+	 */
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	/**
+	 * @return the district
+	 */
+	public String getDistrict() {
+		return district;
+	}
+
+	/**
+	 * @param district the district to set
+	 */
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	/**
+	 * @return the vacType
+	 */
+	public String getVacType() {
+		return vacType;
+	}
+
+	/**
+	 * @param vacType the vacType to set
+	 */
+	public void setVacType(String vacType) {
+		this.vacType = vacType;
+	}
+
+	/**
+	 * @return the dateFirst
+	 */
+	public String getDateFirst() {
+		return dateFirst;
+	}
+
+	/**
+	 * @param dateFirst the dateFirst to set
+	 */
+	public void setDateFirst(String dateFirst) {
+		this.dateFirst = dateFirst;
+	}
+
+	/**
+	 * @return the dateSecond
+	 */
+	public String getDateSecond() {
+		return dateSecond;
+	}
+
+	/**
+	 * @param dateSecond the dateSecond to set
+	 */
+	public void setDateSecond(String dateSecond) {
+		this.dateSecond = dateSecond;
+	}
+
+	/**
+	 * @return the notiDate
+	 */
+	public String getNotiDate() {
+		return notiDate;
+	}
+
+	/**
+	 * @param notiDate the notiDate to set
+	 */
+	public void setNotiDate(String notiDate) {
+		this.notiDate = notiDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((regiNum == null) ? 0 : regiNum.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (regiNum == null) {
+			if (other.regiNum != null)
+				return false;
+		} else if (!regiNum.equals(other.regiNum))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Member [name=");
+		builder.append(name);
+		builder.append(", regiNum=");
+		builder.append(regiNum);
+		builder.append(", contact=");
+		builder.append(contact);
+		builder.append(", district=");
+		builder.append(district);
+		builder.append(", vacType=");
+		builder.append(vacType);
+		builder.append(", dateFirst=");
+		builder.append(dateFirst);
+		builder.append(", dateSecond=");
+		builder.append(dateSecond);
+		builder.append(", notiDate=");
+		builder.append(notiDate);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+	
 }
